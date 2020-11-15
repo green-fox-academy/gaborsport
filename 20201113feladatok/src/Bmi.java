5db8b803f9abc7169735386f02eb8bdf2792c960
gaborsport <67145580+gaborsport@users.noreply.github.com>
2020-11-15 12:37:38 +0100

    Bmi is added

---
+++ 20201113feladatok/src/Bmi.java
@@ -0,0 +1,11 @@
+public class Bmi {
+    public static void main(String[] args) {
+
+        double massInKg = 70.0;
+        double heightInM = 1.78;
+        System.out.println(massInKg / (heightInM*heightInM) );
+
+        // Számíttasd és írasd ki a testtömeg-indexet (BMI)
+        // a változókat használva
+    }
+}

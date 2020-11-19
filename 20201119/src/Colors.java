138b75ce24f01e4253b11118d4935d956a31a442
gaborsport <67145580+gaborsport@users.noreply.github.com>
2020-11-19 20:53:45 +0100

    Colors is added

---
+++ 20201119/src/Colors.java
@@ -0,0 +1,19 @@
+public class Colors {
+    public static void main(String[] args) {
+
+        // - Készíts egy kétdimenziós tömböt
+//   ami megadott színek különböző árnyalatait tartalmazza
+// - A `colors[0]` a zöld árnyalatait tárolja:
+//   `"lime", "forest green", "olive", "pale green", "spring green"`
+// - A `colors[1]` a piros árnyalatait:
+//   `"orange red", "red", "tomato"`
+// - A `colors[2]` pedig a pink árnyalatait:
+//   `"orchid", "violet", "pink", "hot pink"`
+
+        String[][] colors = new String[3][];
+        colors[0] = new String[]{"lime", "forest green", "olive", "pale green","sprig green"};
+        colors[1] = new String[]{"orange red", "red", "tomato"};
+        colors[2] = new String[]{"orchid", "violet", "pink", "hot pink"};
+
+    }
+}

cf1a6702357faf0cde3fc4d8c5755a7a73d4c853
gaborsport <67145580+gaborsport@users.noreply.github.com>
2020-11-19 10:02:55 +0100

    Arrays is added

---
+++ 20201119/src/Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 6};
+
+        // Írasd ki a tömb második elemét
+        System.out.println("A tömb második eleme: " + array[1]);
+
+        // Írasd ki a tömb minden elemét
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
+        }
+    }
+}

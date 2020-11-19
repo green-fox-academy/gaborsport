23e4ec08e62bd100078fe30269554634db6e5f70
gaborsport <67145580+gaborsport@users.noreply.github.com>
2020-11-19 14:10:28 +0100

    4 files are added

---
+++ 20201119/src/ArraysUpload.java
@@ -0,0 +1,21 @@
+public class ArrasyUpload {
+    public static void main(String[] args) {
+
+        // tömb deklarálása és méretének megadása
+        int[] tomb = new int[10];
+
+// tömb feltöltése
+        for( int i = 0; i < tomb.length; i++ )
+        {
+            tomb[i] = (int)(Math.random() * 100) + 1;
+        }
+
+// tömb elemeinek kiíratása
+        for( int i = 0; i < tomb.length; i++ )
+        {
+            System.out.print(tomb[i] + " ");
+        }
+// extra sordobás
+        System.out.println();
+    }
+}

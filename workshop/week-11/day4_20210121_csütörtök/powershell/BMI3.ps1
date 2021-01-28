5f590cbf869069b7b5d4fbd7231e4d132e3c268f
gaborsport <67145580+gaborsport@users.noreply.github.com>
2021-01-28 19:13:15 +0100

    BMI3.ps1 is added

---
+++ workshop/week-11/day4_20210121_csütörtök/powershell/BMI3.ps1
@@ -0,0 +1,18 @@
+﻿clear
+
+Write-Host BMI count
+
+$magassag = Read-Host "Your height (cm)"
+$suly = Read-Host "Your weight (kg)"
+
+$BMI = [double]$suly * 10000 /$magassag /$magassag
+ Write-Host "Your BMI:" $BMI
+
+ if ($BMI -lt 18)
+    { Write-Host "Underweight" }
+
+ elseif ($BMI -gt 18 -and $BMI -lt 25)
+    { Write-Host "Normal"}
+
+ elseif ($BMI -gt 25)
+    { Write-Host "Overweight"}
